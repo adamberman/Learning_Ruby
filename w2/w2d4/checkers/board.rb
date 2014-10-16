a4f3b99b6b1ad9f7e45861e9629ef05084e4bc81
adamberman <adamberman13@gmail.com>
2014-10-16 11:22:17 -0700

    debug current methods

--- w2/w2d4/checkers/board.rb
+++ w2/w2d4/checkers/board.rb
@@ -1,6 +1,8 @@
+require './piece.rb'
+
 class Board
 
-	attr_accessor :rows
+	attr_accessor :grid
 
 	def initialize(populate = true)
 		@grid = Array.new(8) { Array.new(8) }
@@ -8,22 +10,22 @@ class Board
 	end
 
 	def self.in_bounds?(pos)
-		pos.all? { |x| x.between(0, 7) }
+		pos.all? { |x| x.between?(0, 7) }
 	end
 
 	def populate_board
-		8.times do |j|
-			4.times do |i|
-				next if j.between?(3, 4)
-				set_piece(j, i)
+		(0..7).each do |j|
+			next if j == 3 || j == 4
+			(0..3).each do |i|
+				set_piece(i, j)
 			end
 		end
 	end
 
-	def set_piece(y, x)
-		offset = j % 2 == 0 ? 0 : 1
-		color = j >= 5 ? :b : :w
-		pos = [(i * 2) + offset, j]
+	def set_piece(x, y)
+		offset = y % 2 == 0 ? 0 : 1
+		color = y >= 5 ? :b : :w
+		pos = [(x * 2) + offset, y]
 		self[pos] = Piece.new(color, self, pos)
 	end
 
@@ -37,3 +39,8 @@ class Board
 		@grid[y][x] = piece
 	end
 end
+
+board = Board.new(false)
+piece = Piece.new(:b, board, [2,0])
+board[[2,0]] = piece
+p piece.perform_slide([3,-1])
--- w2/w2d4/checkers/piece.rb
+++ w2/w2d4/checkers/piece.rb
@@ -6,13 +6,13 @@ class Piece
 
 	attr_reader :color, :board
 
-	def initialize(color, board, position)
-		@color, @board, @position = color, board, position
+	def initialize(color, board, pos)
+		@color, @board, @pos = color, board, pos
 		@king = false
 	end
 
 	def perform_slide(end_pos)
-		board.in_bounds?(end_pos) && slideable?(end_pos)
+		board.class.in_bounds?(end_pos) && slideable?(end_pos)
 	end
 
 	def slideable?(end_pos)
@@ -21,10 +21,10 @@ class Piece
 
 	def in_sliding_direction?(end_pos)
 		move_directions.each do |direction|
-			possible_pos = [end_pos[0] + direction[0], end_pos[1] + direction[1]]
+			possible_pos = [pos[0] + direction[0], pos[1] + direction[1]]
 			return true if possible_pos == end_pos
 		end
-		
+
 		false
 	end
 
@@ -33,7 +33,7 @@ class Piece
 
 	def move_directions
 		return DIRECTIONS if king
-		return DIRECTIONS[0..1] if :w
+		return DIRECTIONS[0..1] if color == :w
 		DIRECTIONS[2..3]
 	end
 

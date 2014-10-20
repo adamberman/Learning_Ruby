138d3c2d291150dc3ebedf42f22dbf5f08a6ae98
adamberman <adamberman13@gmail.com>
2014-10-19 18:59:26 -0700

    create initialize method

--- poker/spec/player_spec.rb
+++ poker/spec/player_spec.rb
@@ -1,5 +1,11 @@
-relative_require '../lib/player'
+require_relative '../lib/player'
 
 describe Player do
 	let(:player) { Player.new }
+
+	describe "initialize" do
+		it "should initialize with a nil hand" do
+			expect(player.hand).to be_nil 
+		end
+	end
 end
